1a961d6d02be634f44ed51736e98e3de3aa19027
Igor Gomes Lemes <igor.lemes@live.com>
2021-01-09 11:02:25 -0300

    mortagage program copied

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,15 @@
 # mortgage.py
-#
+#Dave has decided to take out a 30-year fixed rate mortgage of $500,000 with Guido’s Mortgage.
+# The interest rate is 5% and the monthly payment is $2684.11.
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+
+while principal > 0:
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+
+print('Total paid', round(total_paid,4))
